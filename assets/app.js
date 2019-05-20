e8415c707af45fda1ff55d6459fd9e68ebd11b97
CB721 <clintbrodar@gmail.com>
2019-05-20 09:03:50 -0700

    Basic structure and psuedo code

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,32 @@
 $("document").ready(function () {
-    
+    //user inputs address
+    //convert address to coordinates - geolocation api
+    //store in firebase
+    //on child added
+    //geolocation api from firebase to placement on map?
+    //columns to input
+        //rent amount
+        //parking cost
+        //washer/dryer in unit?
+        //square footage
+        //quality rating
+        //how far from grocery store
+        //how far from public transit
+        //places within walking distance
+        //open on saturday
+        //open on sunday
+        //additional features
+            //pool
+            //tennis court
+            //basketball court
+            //fitness center
+        //link to their website
+            
+    //assign values for each search parameter
+    //end column that totals all values
+        //if over certain amount, highlight green
+        //else, highlight red
+    //user has ability to update values as needed
 
 
 
