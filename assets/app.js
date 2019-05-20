57cc8f94923e951f64f7f4e98f7ebd0eca2b79d7
CB721 <clintbrodar@gmail.com>
2019-05-20 13:30:45 -0700

    Capture user inputs

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,51 @@
 $("document").ready(function () {
-    //user inputs address
+    //user inputs apartment
+    var name = "";
+    var rent = "";
+    var parking = "";
+    var laundry = true;
+    var sqFoot = "";
+    var grocery = "";
+    var transit = "";
+    var interestPlaces = [];
+    var saturday = true;
+    var sunday = true;
+    var features = [];
+    var websiteLink = "";
+
+    //on click get values from user input
+    $("#userInput").on("click", function (event) {
+        //prevent page from submitting itself
+        event.preventDefault();
+
+        //capture user inputs and store in variables
+        name = $("#name").val().trim();
+        rent = $("#rent").val().trim();
+        parking = $("#parking").val().trim();
+        laundry = $("#laundry").val().trim();
+        sqFoot = $("#sq-foot").val().trim();
+        grocery = $("#grocery").val().trim();
+        transit = $("#transit").val().trim();
+        interestPlaces = $("#interest-places").val().trim();
+        saturday = $("#saturday").val().trim();
+        sunday = $("#sunday").val().trim();
+        features = $("#features").val().trim();
+        websiteLink = $("#website-link").val().trim();
+        console.log(name);
+        console.log(rent);
+        console.log(parking);
+        console.log(laundry);
+        console.log(sqFoot);
+        console.log(grocery);
+        console.log(transit);
+        console.log(interestPlaces);
+        console.log(saturday);
+        console.log(sunday);
+        console.log(features);
+        console.log(websiteLink);
+
+
+    })
     //convert address to coordinates - geolocation api
     //store in firebase
     //on child added
